e1c77aac7f08a4ddd322eeeb8427bc7b8cff71d8
Starch <mrdave67@gmail.com>
2021-01-23 10:23:16 +0000

    update is available

--- index.js
+++ index.js
@@ -1,31 +1,50 @@
 //packages
 require("dotenv").config();
 const axios = require("axios");
+const {google} = require('googleapis');
+const sheets = google.sheets('v4')
+
 //setup
 let data = ["a", "b", "c"];
 let i = 0;
+const auth = new google.auth.GoogleAuth({ //google auth!
+    keyFile: './key.json',
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'], //read & write
+  });
+// set auth as a global default
+google.options({
+    auth: auth
+  });
+
 
 //variables
 const GMAIL_USER = process.env.GMAIL_USER;
 const GMAIL_PASS = process.env.GMAIL_PASS;
 const GOOGLE_API = process.env.GOOGLE_API;
 const SP_ID = process.env.SPREADSHEET_ID;
-
 const URL =
   "https://sheets.googleapis.com/v4/spreadsheets/spreadsheetId/values/Sheet1!A1:D5";
 
-const app = async () => {
-  try {
-    const response = await axios.get(
-      `https://sheets.googleapis.com/v4/spreadsheets/${SP_ID}/values/Sheet1!C6:E6`,
-      {
-        params: {
-          key: GOOGLE_API,
-        },
-      }
-    );
 
-    console.log(response.data.values);
+//functions
+
+const get = async () => {
+  try {
+    sheets.spreadsheets.values.get(
+        {
+          auth: auth,
+          spreadsheetId: SP_ID,
+          range: 'Sheet1!C6:E6',
+        },
+        (err, res) => {
+          if (err) {
+            console.error('The API returned an error.');
+            throw err;
+          }
+          const rows = res.data.values;
+          console.log(rows)
+        }
+      )
   } catch (e) {
     console.log(e.response);
   }
@@ -45,29 +64,37 @@ mailSender = async () => {
 const update = async () => {
   try {
 
-    const response = await axios.put(
-      `https://sheets.googleapis.com/v4/spreadsheets/${SP_ID}/values/Sheet1!C6:C6?valueInputOption=USER_ENTERED`,
-      {
-        range: "Sheet1!C6:C6",
-        majorDimension: "ROWS",
-        values: [["NodeJs"]],
-
-    },{
-        params: {
-          key: GOOGLE_API,
+    const response = await sheets.spreadsheets.values.update(
+        {
+          spreadsheetId: SP_ID,
+          range: 'Sheet1!C6',
+          valueInputOption: 'USER_ENTERED',
+         
+          requestBody: {
+            values: [
+              ['Email Sent!']
+            ],
+          }
         }
-    }
-    );
+      );
+        console.log(response.status);
 
-    console.log(response.data.values);
   } catch (e) {
-    console.log(e.response);
+    console.log(e);
   }
+  console.log('end')
 };
 
-console.log("\nHi, \nApp is going to send emails");
 
+
+//run
+
+console.log("\nHi, \nApp is going to start...");
+
+get();
 // update();
-app();
+
+
+// app();
 
 // const starter = setInterval(mailSender, 3000);
